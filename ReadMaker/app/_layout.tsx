74ca640ef22a73fdc027e3ffe651ca2bc59089dd
oto1720 <kotaro17206@icloud.com>
2025-08-11 16:17:29 +0900

    feat:スプラッシュ画面を追加

--- ReadMaker/app/_layout.tsx
+++ ReadMaker/app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="splash" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="reader" options={{ headerShown: false }} />
     </Stack>
--- ReadMaker/app/index.tsx
+++ ReadMaker/app/index.tsx
@@ -2,6 +2,6 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return <Redirect href="/(tabs)/home" />;
+  return <Redirect href="/splash" />;
 }
 
